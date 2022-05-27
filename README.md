6cb9e9c714a26376b184927c4b1a2d1a03a92cdc
TicTac1602 <63048103+TicTac1602@users.noreply.github.com>
2022-05-27 13:42:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+On this challenge I really tried to make a pixel perfect look by using fixed dimensions. I know that it is not the best way to work as responsiveness is key nowadays. But trying to find the perfect fit was something i rarely made.
+
+### Continued development
+
+I plan to make a lot of those project in the future to be as good as I can on basic web app making. Then I will learn a JS library to make some VueJs or ReactJS.
+
+## Author
+
+- Website - [Emil Toulouse](https://emiltoulouse.com)
+- Frontend Mentor - [@TicTac1602](https://www.frontendmentor.io/profile/TicTac1602)
+- Twitter - [@TicTac1602](https://www.twitter.com/TicTac1602)
---
+++ index.js
@@ -0,0 +1,129 @@
+// gets user input
+const bill = document.getElementById("bill-amount");
+const people = document.getElementById("people-number");
+const resetButton = document.getElementById("reset");
+const errorPeople = document.getElementById("emsg-people");
+const errorBill = document.getElementById("emsg-bill");
+const peopleSection = document.getElementById("people-section");
+const billSection = document.getElementById("bill-section");
+
+
+// default element on result display
+document.getElementById("amount").innerHTML = '$0.00';
+document.getElementById("total-amount").innerHTML = '$0.00';
+
+//gets info from html
+const percent = document.querySelectorAll(".percent");
+const custom = document.getElementById("custom");
+
+let billInput;
+let peopleInput;
+let tipPercent;
+resetButton.disabled = true;
+
+// checks whether input is more than 0
+function checkBill () {
+    if (typeof +bill.value !== 'number' || isNaN(+bill.value) || +bill.value <= 0) {
+        billSection.classList.add("error");
+        errorBill.innerHTML = "Can't be zero";
+    } else {
+        
+        billSection.classList.remove("error");
+        errorBill.innerHTML = "";
+    }
+    update()
+}
+
+function checkPeople () {
+    if (typeof +people.value !== 'number' || isNaN(+people.value) || +people.value <= 0) {
+        peopleSection.classList.add("error");
+        errorPeople.innerHTML = "Can't be zero";
+        false;
+    } else { 
+        peopleSection.classList.remove("error");
+        errorPeople.innerHTML = "";
+        true;
+    }
+    update()
+}
+
+bill.addEventListener('change', (element) => { 
+    billInput = element.target.value
+    update()
+})
+
+people.addEventListener('change', (element) => {
+    peopleInput = element.target.value
+    update()
+})
+
+percent.forEach(element => {
+    element.addEventListener('click', function (e) {
+        percent.forEach(element => {
+            element.classList.remove('percent-active')
+        } )
+        
+        element.classList.add('percent-active')
+        tipPercent = +e.target.innerHTML.replace("%", "") / 100
+        document.getElementById("custom").value = '';
+        update()
+    })
+})
+
+custom.addEventListener('change', (cus) => {
+    percent.forEach(element => {
+        element.classList.remove('percent-active')
+    } )
+    tipPercent = +cus.target.value.replace("%", "") / 100
+    update()
+})
+
+
+function update () {
+    let tipPerPerson 
+    let totalPerPerson;
+    resetButton.disabled = false;
+    if (billInput !== undefined && tipPercent !== undefined && peopleInput !== undefined) {
+        if (isNaN(peopleInput) || peopleInput === '0') {
+            tipPerPerson = '0.00'
+            totalPerPerson = '0.00'
+        } else {
+            tipPerPerson = billInput * tipPercent / peopleInput
+            totalPerPerson = billInput / peopleInput + tipPerPerson
+        }
+
+        document.getElementById("amount").innerHTML = '$' + tipPerPerson.toFixed(2);
+        document.getElementById("total-amount").innerHTML = '$' + totalPerPerson.toFixed(2);
+    } else {
+        document.getElementById("amount").innerHTML
+        document.getElementById("total-amount").innerHTML
+    }
+
+    resetButton.onclick = () => {   
+
+        document.getElementById("bill-amount").value = '';
+        billInput = undefined;
+        document.getElementById("people-number").value = '';
+        peopleInput = undefined;
+        document.querySelectorAll(".percent").value = '';
+        tipPercent = undefined;
+        document.getElementById("custom").value = '';
+        document.getElementById("custom").placeholder = 'Custom';
+
+        percent.forEach(element => {
+            element.classList.remove('percent-active')
+
+        } )
+        document.getElementById("amount").innerHTML = '$0.00';
+        document.getElementById("total-amount").innerHTML = '$0.00';
+
+        peopleSection.classList.remove("error");
+        errorPeople.innerHTML = "";
+
+        billSection.classList.remove("error");
+        errorBill.innerHTML = "";
+        
+        resetButton.disabled = true;
+
+    }
+}
